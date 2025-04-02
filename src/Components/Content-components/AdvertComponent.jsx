2640dba2ae673246c219e44040f2d4f014a8da69
Kuba <jakub.kulig2004@gmail.com>
2025-04-02 20:39:00 +0200

    Added adverts

--- src/Components/Content-components/AdvertComponent.jsx
+++ src/Components/Content-components/AdvertComponent.jsx
@@ -1,15 +1,60 @@
 import '../../component-styles/Content-components/AdvertComponent.css'
-import { SlArrowLeft } from "react-icons/sl";
+import { SlArrowLeft, SlArrowRight} from "react-icons/sl";
+import { useState, useRef } from 'react';
+
 function AdvertComponent() {
+
+    const adverts = ['./ad1.jpg', './ad2.jpg', './ad3.jpg'];
+
+    const [index, setIndex] = useState(0);
+
+    const myRef = useRef(null)
+
+    function swapAdvertRight() {
+        if (index < adverts.length - 1) {
+
+            setIndex(index + 1);
+
+        }   else {
+
+            setIndex(0);
+
+        };
+        
+    };
+
+    function swapAdvertLeft() {
+
+        if (index > 0) {
+
+            setIndex(index - 1);
+        };
+    };
 
     return (
         <div
            className="advert-flex">
             <a
+            className='go-to-next-advert'
+            onClick={swapAdvertRight}
+            >
+                <SlArrowRight
+                size={100}
+                />
+                </a>
+
+            <img src={adverts[index]}
+            ref={myRef}>
+            </img>
+
+            <a
             className='go-to-previous-advert'
-            ></a>
-               <img src='./nie-wolno-nawet-troche-pomarzyc.jpg.webp'>
-               </img>
+            onClick={swapAdvertLeft}
+            >
+                <SlArrowLeft
+                size={100}
+                />
+                </a>
         </div>
         
     )
