b8fb028a6cee25c428965febc4c34f3c7cf2f620
Kuba <jakub.kulig2004@gmail.com>
2025-04-13 23:12:41 +0200

    add bin

--- src/Components/Cart-page-components/CartProduct.jsx
+++ src/Components/Cart-page-components/CartProduct.jsx
@@ -1,11 +1,23 @@
 import QuantityCounter from "./QuantityCounter"
 import './../../component-styles/Cart-components/CartProduct.css'
+import { RiDeleteBin6Line } from "react-icons/ri";
 export default function CartProduct({ img, price, count, name, id }) {
 
     return (
         <div
         className="cart-item-flex"
         >
+            <div
+            className="bin-icon"
+            >
+                <RiDeleteBin6Line
+                size={40}
+                color="rgb(214, 82, 82)"
+                />
+                <div className="bin-pop-up-info">
+                    Remove Product from your Cart
+                </div>
+            </div>
             <div className="img-div">
                 <img src= {`./Electronics/${img}`} className="cart-product-img"/>
             </div>
